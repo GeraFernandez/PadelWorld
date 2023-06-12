6be6bf9e079615d3ea12e8b18e6d67660f9ac325
cruzac1985 <accruzguevara@gmail.com>
2023-06-12 19:57:17 -0300

    cambios php calendario

--- pages/profesores.php
+++ pages/profesores.php
@@ -302,60 +302,60 @@
     <div class="calendar">
       <div class="month">
         <i class="fas fa-angle-left prev"></i>
-        <div class="date">december 2015</div>
+        <div class="date">Junio 2023</div>
         <i class="fas fa-angle-right next"></i>
       </div>
       <div class="weekdays">
-        <div>Sun</div>
-        <div>Mon</div>
-        <div>Tue</div>
-        <div>Wed</div>
-        <div>Thu</div>
-        <div>Fri</div>
-        <div>Sat</div>
+        <div>Dom</div>
+        <div>Lun</div>
+        <div>Mar</div>
+        <div>Miér</div>
+        <div>Jue</div>
+        <div>Vier</div>
+        <div>Sáb</div>
       </div>
       <div class="days"></div>
       <div class="goto-today">
         <div class="goto">
           <input type="text" placeholder="mm/yyyy" class="date-input" />
-          <button class="goto-btn">Go</button>
+          <button class="goto-btn">Ir</button>
         </div>
-        <button class="today-btn">Today</button>
+        <button class="today-btn">Hoy</button>
       </div>
     </div>
   </div>
   <div class="right">
     <div class="today-date">
-      <div class="event-day">wed</div>
-      <div class="event-date">12th december 2022</div>
+      <div class="event-day">Lun</div>
+      <div class="event-date">12 de Diciembre</div>
     </div>
     <div class="events"></div>
     <div class="add-event-wrapper">
       <div class="add-event-header">
-        <div class="title">Add Event</div>
+        <div class="title">Agregar Evento</div>
         <i class="fas fa-times close"></i>
       </div>
       <div class="add-event-body">
         <div class="add-event-input">
-          <input type="text" placeholder="Event Name" class="event-name" />
+          <input type="text" placeholder="Nombre del Evento" class="event-name" />
         </div>
         <div class="add-event-input">
           <input
             type="text"
-            placeholder="Event Time From"
+            placeholder="Hora de inicio"
             class="event-time-from"
           />
         </div>
         <div class="add-event-input">
           <input
             type="text"
-            placeholder="Event Time To"
+            placeholder="Hora de fin"
             class="event-time-to"
           />
         </div>
       </div>
       <div class="add-event-footer">
-        <button class="add-event-btn">Add Event</button>
+        <button class="add-event-btn">Guardar</button>
       </div>
     </div>
   </div>
@@ -365,6 +365,8 @@
 </div>
 <script src="/js/scrips.js"></script>
 </section>
+
+
 
 
 
