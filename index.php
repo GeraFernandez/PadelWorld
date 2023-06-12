2700241729acd9b12d973e5041360396e9d55ea9
camila graziani <100491743+cgraziani@users.noreply.github.com>
2023-06-12 20:45:06 -0300

    cambios

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<?php
-if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-  $uri = 'https://';
-} else {
-  $uri = 'http://';
-}
-$uri .= $_SERVER['HTTP_HOST'];
-header('Location: '.$uri.'/dashboard/');
 
-
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -19,7 +9,7 @@ header('Location: '.$uri.'/dashboard/');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <!-- favicon -->
-    <link rel="shortcut icon" href="/img/logo-pelota.png">
+    <link rel="shortcut icon" href="../img/logo-pelota.png">
 
    <!-- iconos -->
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
@@ -28,7 +18,7 @@ header('Location: '.$uri.'/dashboard/');
     <!-- bootsrap css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="/styles/estilos.php">
+    <link rel="stylesheet" href="./styles/estilos.css">
 
     
     <title>Padel World - Registro  </title>
@@ -38,37 +28,37 @@ header('Location: '.$uri.'/dashboard/');
   <!-- NAV -->
   <nav class="navbar navbar-dark navbar-expand-lg" style="background-color: #040738;">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#"><img src="/img/logo-pelota.png" alt="" srcset="" width=" 35px"></a> <!-- modificar con css, cprregir detaslle -->
+      <a class="navbar-brand" href="#"><img src="../img/logo-pelota.png" alt="" srcset="" width=" 35px"></a> <!-- modificar con css, cprregir detaslle -->
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item"><!-- modif agregar links a paginas -->
-            <a class="nav-link active" aria-current="page" href="/index.php">Inicio</a>
+            <a class="nav-link active" aria-current="page" href="./index.php">Inicio</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/pages/jugadores.php">Jugadores</a>
+            <a class="nav-link" href="./pages/jugadores.php">Jugadores</a>
           </li>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/pages/clubes.php">Clubes</a>
+          <a class="nav-link" href="./pages/clubes.php">Clubes</a>
         </li>
         </li>
           <li class="nav-item">
-            <a class="nav-link" href="/pages/profesores.php">Profesores</a>
+            <a class="nav-link" href="./pages/profesores.php">Profesores</a>
           </li>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/pages/torneos.php">Torneos</a>
+          <a class="nav-link" href="./pages/torneos.php">Torneos</a>
         </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Otros
             </a>
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="/pages/novedades.php">Novedades</a></li>
-              <li><a class="dropdown-item" href="/pages/sobre-nosotros.php">Sobre nosotros</a></li>
+              <li><a class="dropdown-item" href="./pages/novedades.php">Novedades</a></li>
+              <li><a class="dropdown-item" href="./pages/sobre-nosotros.php">Sobre nosotros</a></li>
               <li><hr class="dropdown-divider"></li>
               <li><a class="dropdown-item" href="#contacto">Contacto</a></li>
 
@@ -86,8 +76,8 @@ header('Location: '.$uri.'/dashboard/');
             <i class="bi bi-person-circle display-6"></i>
           </a>
           <ul class="dropdown-menu dropdown-menu-end">
-            <li><a class="dropdown-item" href="/pages/login.php">Iniciar sesión</a></li>
-            <li><a class="dropdown-item" href="/pages/registrarse.php">Registrarse</a></li>
+            <li><a class="dropdown-item" href="./pages/login.php">Iniciar sesión</a></li>
+            <li><a class="dropdown-item" href="./pages/registrarse.php">Registrarse</a></li>
           </ul>
         </div>
         
@@ -108,20 +98,20 @@ header('Location: '.$uri.'/dashboard/');
     </div><!-- modif ver transiciones -->
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="/img/fondo-padel2.jpg" class="d-block w-100 " alt="carousel1"> <!-- MODIF ver aca para ajustar responsive -->
+        <img src="./img/fondo-padel2.jpg" class="d-block w-100 " alt="carousel1"> <!-- MODIF ver aca para ajustar responsive -->
         <div class="carousel-caption ">
           <h5 class="carousel-title">Organización de torneos, Gestion y reservas de turnos, Profesores y Prestadores de Servicios de padel.</h5>
           </div>
       
       </div>
       <div class="carousel-item">
-        <img src="/img/agenda-world-padel-tour-2023.jpg" class=" w-100" alt="carousel2">
+        <img src="./img/agenda-world-padel-tour-2023.jpg" class=" w-100" alt="carousel2">
         <div class="carousel-caption d-none d-md-block">
           <h5 class="carousel-title">Organización de torneos, Gestion y reservas de turnos, Profesores y Prestadores de Servicios de padel.</h5>
         </div>
       </div>
-      <div class="carousel-item">sd
-        <img src="/img/pxfuel.jpg" class="d-block w-100" alt="carousel3">
+      <div class="carousel-item">
+        <img src="./img/pxfuel.jpg" class="d-block w-100" alt="carousel3">
         <div class="carousel-caption d-none d-md-block">
           <h5 class="carousel-title">Organización de torneos, Gestion y reservas de turnos, Profesores y Prestadores de Servicios de padel.</h5>
         </div>
