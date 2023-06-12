bcaa2b7dfae9ec3ee280dbe25a48253ef4defa96
cruzac1985 <accruzguevara@gmail.com>
2023-06-12 19:45:02 -0300

    cambios img y html

--- pages/profesores.php
+++ pages/profesores.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <!-- favicon -->
-    <link rel="shortcut icon" href="/img/logo-pelota.png">
+    <link rel="shortcut icon" href="../img/logo-pelota.png">
 
    <!-- iconos -->
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
@@ -32,37 +32,38 @@
   <!-- NAV -->
   <nav class="navbar navbar-dark navbar-expand-lg" style="background-color: #040738;">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#"><img src="/img/logo-pelota.png" alt="" srcset="" width=" 35px"></a> <!-- modificar con css, cprregir detaslle -->
+      <a class="navbar-brand" href="#"><img src="../img/logo-pelota.png" alt="" srcset="" width=" 35px"></a>
+       <!-- modificar con css, cprregir detaslle -->
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item"><!-- modif agregar links a paginas -->
-            <a class="nav-link" aria-current="page" href="/index.html">Inicio</a>
+            <a class="nav-link" aria-current="page" href="../index.php">Inicio</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/pages/jugadores.html">Jugadores</a>
+            <a class="nav-link" href="../pages/jugadores.php">Jugadores</a>
           </li>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/pages/clubes.html">Clubes</a>
+          <a class="nav-link" href="../pages/clubes.php">Clubes</a>
         </li>
         </li>
           <li class="nav-item">
-            <a class="nav-link active" href="/pages/profesores.html">Profesores</a>
+            <a class="nav-link active" href="../pages/profesores.php">Profesores</a>
           </li>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/pages/torneos.html">Torneos</a>
+          <a class="nav-link" href="../pages/torneos.php">Torneos</a>
         </li>
           <li class="nav-item dropdown">
             <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Otros
             </a>
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="/pages/novedades.html">Novedades</a></li>
-              <li><a class="dropdown-item" href="/pages/sobre-nosotros.html">Sobre nosotros</a></li>
+              <li><a class="dropdown-item" href="../pages/novedades.php">Novedades</a></li>
+              <li><a class="dropdown-item" href="../pages/sobre-nosotros.php">Sobre nosotros</a></li>
               <li><hr class="dropdown-divider"></li>
               <li><a class="dropdown-item" href="#">Contacto</a></li>
             </ul>
@@ -158,7 +159,7 @@
       <div class="row ">
         <div class="col-md-4 ">
           <div class="card  ">
-            <img src="/img/pxfuel.jpg" class="card-img-top" alt="...">            
+            <img src="../img/pxfuel.jpg" class="card-img-top" alt="...">            
             <div class="card-body">
               <h5 class="card-title">Profe 1</h5>
               <p class="card-text">Breve descripción del profesor.</p>
@@ -173,7 +174,7 @@
         </div>
         <div class="col-md-4">
           <div class="card">
-            <img src="/img/pxfuel.jpg" class="card-img-top" alt="...">
+            <img src="../img/pxfuel.jpg" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Profe 2</h5>
               <p class="card-text">Breve descripción del profesor.</p>
@@ -188,7 +189,7 @@
         </div>
         <div class="col-md-4">
           <div class="card">
-            <img src="/img/pxfuel.jpg" class="card-img-top" alt="...">
+            <img src="../img/pxfuel.jpg" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Profe 3</h5>
               <p class="card-text">Breve descripción del profesor.</p>
@@ -202,7 +203,7 @@
         </div>  
         <div class="col-md-4">
           <div class="card">
-            <img src="/img/pxfuel.jpg" class="card-img-top" alt="...">
+            <img src="../img/pxfuel.jpg" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Profe 4</h5>
               <p class="card-text">Breve descripción del profesor.</p>
@@ -216,7 +217,7 @@
         </div>  
         <div class="col-md-4">
           <div class="card">
-            <img src="/img/pxfuel.jpg" class="card-img-top" alt="...">
+            <img src="../img/pxfuel.jpg" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Profe 5</h5>
               <p class="card-text">Breve descripción del profesor.</p>
@@ -230,7 +231,7 @@
         </div>   
         <div class="col-md-4">
           <div class="card">
-            <img src="/img/pxfuel.jpg" class="card-img-top" alt="...">
+            <img src="../img/pxfuel.jpg" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Profe 6</h5>
               <p class="card-text">Breve descripción del profesor.</p>
